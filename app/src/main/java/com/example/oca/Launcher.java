0755134844f7a2b4b03e2d4bc2c9cb26b513c34d
Janis <janis.fong@yahoo.com>
2023-06-02 00:11:05 +0800

    Commit files for Slider

--- app/src/main/java/com/example/oca/Launcher.java
+++ app/src/main/java/com/example/oca/Launcher.java
@@ -51,7 +51,7 @@ public class Launcher extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent (Launcher.this, Login.class);
+                Intent intent = new Intent (Launcher.this, PageSlider.class);
                 Pair[] pairs = new Pair [2];
                 pairs[0] = new Pair<View,String>(logo,"logo_image");
                 pairs[1] = new Pair<View,String>(slogan,"logo_name");
